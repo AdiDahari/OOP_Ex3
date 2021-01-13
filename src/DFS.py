2bc67f71de757b09136ca66046ce8a5d3ab3e9dd
Adi Dahari <dahariadi16@gmail.com>
2021-01-13 12:52:00 +0200

    minor syntax fixes

--- src/DFS.py
+++ src/DFS.py
@@ -27,14 +27,17 @@ def depth_first_search(g, n, low_link, id_dict, scc_list, scc_set, itr):
             min_link = low_link[n]
             for neighbor in g.all_out_edges_of_node(n):
                 if neighbor not in scc_set:
-                    low_link[n] = min(low_link[n], low_link[neighbor])
+                    if low_link[n] > low_link[neighbor]:
+                        low_link[n] = low_link[neighbor]
             if low_link[n] is not id_dict[n]:
                 if low_link[n] not in scc:
                     scc[low_link[n]] = []
                 scc[low_link[n]].extend(scc[min_link])
                 for i in scc[min_link]:
-                    low_link[i.key] = low_link[n]
+                    key = i.key
+                    low_link[key] = low_link[n]
             else:
                 scc_list.append(scc[low_link[n]])
-                scc_set.update([curr.key for curr in scc[low_link[n]]])
+                for i in scc[low_link[n]]:
+                    scc_set.update([i.key])
             s.pop()
